635a678e06577b60e040539ab7d9ea91919cb6c6
SigmaVX <tonywible@gmail.com>
2018-04-08 14:01:31 -0400

    added small css updates for index

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,10 @@
+
 $(".bitBucket").on("click", function(event){
     event.preventDefault();
     $("#bitBucketModal").modal('show');
 });
 
- console.log("working");
-
-
-
-// Hammer JS
+// Hammer JS - used to help on mobile screens
 $('.box').each(function(i, element){
     // we save a jQuery wrapped version here in the closure for callback.
     var $element = $(element);
