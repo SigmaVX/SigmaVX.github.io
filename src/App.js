ff3d60d226bfebc5ddf9b52b6a39f999dbd77a51
SigmaVX <tonywible@gmail.com>
2018-07-27 08:25:37 -0400

    react deploy

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import Home from './containers/Home';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div>
+        <Router>
+          <div>
+            <Home/>
+          </div>
+        </Router>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = (props) => (
+      
+    <footer className="text-center">
+        Tony Wible<br/>&copy; 2018 
+    </footer>
+    
+)
+
+
+export default Footer;
