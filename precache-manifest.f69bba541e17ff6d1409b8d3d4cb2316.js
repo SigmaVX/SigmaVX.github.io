44af47d36bf6089e1e76dd6211fd02af5b02256b
Tony Wible <TonyWible@Tonys-MacBook-Pro.local>
2019-03-05 12:19:03 -0500

    Updates

---
+++ precache-manifest.f69bba541e17ff6d1409b8d3d4cb2316.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "dea1866e211e3b44d635f53bd9576071",
+    "url": "/static/media/train.dea1866e.jpg"
+  },
+  {
+    "revision": "f423dcebbe5833720aa2",
+    "url": "/static/js/0.f423dceb.chunk.js"
+  },
+  {
+    "revision": "37fee0efc1e2b479cfef",
+    "url": "/static/js/main.37fee0ef.chunk.js"
+  },
+  {
+    "revision": "4edef709328419ca939d",
+    "url": "/static/js/runtime~main.4edef709.js"
+  },
+  {
+    "revision": "57f03730994ec663a19b",
+    "url": "/static/js/3.57f03730.chunk.js"
+  },
+  {
+    "revision": "7e1b38b60534fb5da770d8293f7cb431",
+    "url": "/static/media/purplebike.7e1b38b6.jpg"
+  },
+  {
+    "revision": "70a9f62f84fa89992c16",
+    "url": "/static/js/4.70a9f62f.chunk.js"
+  },
+  {
+    "revision": "9e61d1b622f659e2a1f048a8dc7f56bd",
+    "url": "/static/media/tony.9e61d1b6.jpg"
+  },
+  {
+    "revision": "c09d01d33fafbe097f68",
+    "url": "/static/js/5.c09d01d3.chunk.js"
+  },
+  {
+    "revision": "fbb9049d9d574a98e053a9c1321900c7",
+    "url": "/static/media/game_roster.fbb9049d.jpg"
+  },
+  {
+    "revision": "6580b47614980262f6728b16e0ca2836",
+    "url": "/static/media/mobile.6580b476.jpg"
+  },
+  {
+    "revision": "58cef30a37762e2a69378f78eb1a974c",
+    "url": "/static/media/alerts_me.58cef30a.jpg"
+  },
+  {
+    "revision": "78b3980d59b9351bb6ca2dfec9c3bb68",
+    "url": "/static/media/tilt.78b3980d.jpg"
+  },
+  {
+    "revision": "2adb9a7823ca1788d464727771d414aa",
+    "url": "/static/media/agile_flow.2adb9a78.jpg"
+  },
+  {
+    "revision": "7a2a02eb56c7aad0fb05e9ba2a4ec2a8",
+    "url": "/static/media/react_burger.7a2a02eb.png"
+  },
+  {
+    "revision": "9a6ede02bf8eb48f00f00e734e477528",
+    "url": "/static/media/cortex.9a6ede02.jpg"
+  },
+  {
+    "revision": "c37dcfc64878bd0ba008b53b8e934f5e",
+    "url": "/static/media/garyvee.c37dcfc6.jpg"
+  },
+  {
+    "revision": "e11c28745ce588a3017cc0356db2492d",
+    "url": "/static/media/opine.e11c2874.jpg"
+  },
+  {
+    "revision": "a8e356308ace90170321cf9d5b99c302",
+    "url": "/static/media/giphy.a8e35630.jpg"
+  },
+  {
+    "revision": "3afcfb58d1051cd39057457afdd0f76e",
+    "url": "/static/media/yellow-flower3.3afcfb58.jpg"
+  },
+  {
+    "revision": "37a3c75afc6da352eebcc5bf822b46ef",
+    "url": "/static/media/chronos.37a3c75a.jpg"
+  },
+  {
+    "revision": "d88e00cbb0b34821dca3f5a6bb5674f7",
+    "url": "/static/media/rpg_game.d88e00cb.jpg"
+  },
+  {
+    "revision": "6165df14e721723a5410bc7d3fb617e4",
+    "url": "/static/media/toon.6165df14.jpg"
+  },
+  {
+    "revision": "a359c8af4c19da227b753ea27c69027a",
+    "url": "/static/media/team_finder.a359c8af.jpg"
+  },
+  {
+    "revision": "720b224e6b96c319a6350bfd88ac1ac0",
+    "url": "/static/media/nyt.720b224e.jpg"
+  },
+  {
+    "revision": "d2d961464623de5b3bef56089782fe2f",
+    "url": "/static/media/gbay.d2d96146.jpg"
+  },
+  {
+    "revision": "a0c26cd0b06b0d7ae5eeedc6c367ea77",
+    "url": "/static/media/lardac.a0c26cd0.jpg"
+  },
+  {
+    "revision": "cbfee072281574ae301905f9ed919275",
+    "url": "/static/media/purplesky.cbfee072.jpg"
+  },
+  {
+    "revision": "e616ff26f39bdc73824ab0cc7feb00ff",
+    "url": "/static/media/purplewall.e616ff26.jpg"
+  },
+  {
+    "revision": "6ffecbb0f26351ddfa1eb46f0d55f611",
+    "url": "/static/media/purpleflower.6ffecbb0.jpg"
+  },
+  {
+    "revision": "826ebb58d71f736a8dfa93e2654719ee",
+    "url": "/static/media/purplerain.826ebb58.jpg"
+  },
+  {
+    "revision": "3eb0a082211006ba5e2c2340e89200d5",
+    "url": "/static/media/purple-storm-raw.3eb0a082.jpg"
+  },
+  {
+    "revision": "321cdb6f50e2b91c811a719b300d2403",
+    "url": "/static/media/purple-lake-raw.321cdb6f.jpg"
+  },
+  {
+    "revision": "5166db24cc8f9f8016042763d9b9bcc7",
+    "url": "/static/media/purplegrass.5166db24.jpg"
+  },
+  {
+    "revision": "37fee0efc1e2b479cfef",
+    "url": "/static/css/main.f1f37fae.chunk.css"
+  },
+  {
+    "revision": "c09d01d33fafbe097f68",
+    "url": "/static/css/5.be28b87c.chunk.css"
+  },
+  {
+    "revision": "70a9f62f84fa89992c16",
+    "url": "/static/css/4.c6c04d80.chunk.css"
+  },
+  {
+    "revision": "ec1b9c2cc623c130dafd8d44804394d2",
+    "url": "/index.html"
+  }
+];
